4b968df6603753dd1caf446c2bc5ebb9036ff36d
adrianosilvavianna <adriano_vianna@outlook.com>
2018-06-06 17:25:06 -0300

    fix

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Contact;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+    private $database = 'contacts';
+
+    private function createContact() : Contact
+    {
+        return factory(Contact::class)->create();
+    }
+
+    private function getArrayToUpdate() : array
+    {
+        return [
+            'name' => "Um nome teste",
+            'last_name' => "Um sobrenome teste",
+            'email' => 'um email test',
+            'phone' => "um telefone test"
+        ];
+    }
+
+    private function getArrayCompareDatabase(Contact $contact) : array
+    {
+        return [
+            'name'          => $contact->name,
+            'last_name'     => $contact->last_name,
+            'email'         => $contact->email,
+            'phone'         => $contact->phone,
+            'created_at'    => $contact->created_at,
+            'updated_at'    => $contact->updated_at,
+            'deleted_at'    => $contact->deleted_at,
+        ];
+    }
+
+    public function testCanBeCreated() : void
+    {
+        $contact = $this->createContact();
+        $this->assertDatabaseHas($this->database, $this->getArrayCompareDatabase($contact));
+    }
+
+    public function testCanBeUpdated() : void
+    {
+        $contact = $this->createContact();
+        $result = $contact->update($this->getArrayToUpdate());
+        $this->assertTrue($result);
+    }
+
+    public function testCanBeDeleted() : void
+    {
+        $contact = $this->createContact();
+        $contact->delete();
+        $contact->refresh();
+        $this->assertTrue($contact->trashed());
+        $this->assertSoftDeleted($this->database, $this->getArrayCompareDatabase($contact));
+    }
+}
---
+++ tests/Feature/MessageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Contact;
+use App\Models\Message;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MessageTest extends TestCase
+{
+    private $database = 'messages';
+
+    private function createMessage() : Message
+    {
+        return factory(Message::class)->create();
+    }
+
+    private function getArrayToUpdate() : array
+    {
+        return [
+            'description' => "Uma descrição teste",
+        ];
+    }
+
+    private function getArrayCompareDatabase(Message $message) : array
+    {
+        return [
+            'description' => $message->description,
+        ];
+    }
+
+    public function testCanBeCreated() : void
+    {
+        $message = $this->createMessage();
+        $this->assertDatabaseHas($this->database, $this->getArrayCompareDatabase($message));
+    }
+
+    public function testCanBeUpdated() : void
+    {
+        $message = $this->createMessage();
+        $result = $message->update($this->getArrayToUpdate());
+        $this->assertTrue($result);
+    }
+
+    public function testCanBeDeleted() : void
+    {
+        $message = $this->createMessage();
+        $message->delete();
+        $message->refresh();
+        $this->assertTrue($message->trashed());
+        $this->assertSoftDeleted($this->database, $this->getArrayCompareDatabase($message));
+    }
+}
