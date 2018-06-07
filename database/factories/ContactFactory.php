99eac1593df4fc4500daa9ebdd07cf256a9e83f3
adriano-pc <adriano_vianna@outlook.com>
2018-06-07 10:36:09 -0300

    fix correcao factory

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -6,7 +6,7 @@ $factory->define(\App\Models\Contact::class, function (Faker $faker) {
     return [
         'name'      => $faker->name,
         'last_name' => $faker->lastName,
-        'email'     =>$faker->email,
-        'phone'     => $faker->phoneNumber
+        'email'     =>$faker->unique()->emailSafe,
+        'phone'     => $faker->unique()->phoneNumber
     ];
 });
